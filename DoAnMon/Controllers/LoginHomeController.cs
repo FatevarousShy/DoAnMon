4b80677d79858407326a37624c5c5f0eb205e73b
FatevarousShy <86491217+FatevarousShy@users.noreply.github.com>
2021-07-01 15:34:29 +0700

    Làm Bài Đê

---
+++ DoAnMon/Controllers/LoginHomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnMon.Controllers
+{
+    public class LoginHomeController : Controller
+    {
+        // GET: LoginHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TestBank()
+        {
+            ViewBag.Message = "Your TestBank page.";
+
+            return View();
+        }
+    }
+}
