abd64781a60cdec4ab678f85d446bb7139ed3a17
ekit1312 <86712813+ekit1312@users.noreply.github.com>
2021-07-01 23:34:43 -0700

    <3

--- DoAnMon/Controllers/HomeController.cs
+++ DoAnMon/Controllers/HomeController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DoAnMon.Models;
 
 namespace DoAnMon.Controllers
 {
     public class HomeController : Controller
     {
+        WebTNDataContext db = new WebTNDataContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Trang Chủ.";
 
             return View();
         }
+        [HttpGet]
 
         public ActionResult About()
         {
@@ -34,10 +38,76 @@ namespace DoAnMon.Controllers
 
             return View();
         }
+        [HttpPost]
+
+        public ActionResult SignUp(FormCollection collection, TaiKhoan kh)
+        {
+            var tendn = collection["TenDN"];
+            var matkhau = collection["MatKhau"];
+            var email = collection["Email"];
+
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phai Nhap Ten Dang Nhap";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phai nhap mat khau";
+            }
+            if (string.IsNullOrEmpty(email))
+            {
+                ViewData["loi3"] = "Email khong duoc bo trong";
+            }
+            else
+            {
+                //Gán giá trị cho đối tượng được tạo mới (kh)
+                kh.TenDangNhap = tendn;
+                kh.MatKhau = matkhau;
+                kh.Email = email;
+
+                db.TaiKhoans.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("SignIn");
+            }
+            return this.SignUp();
+        }
+        [HttpGet]
+
         public ActionResult SignUp()
         {
             ViewBag.Message = "Trang Đăng Ký.";
 
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult SignIn(FormCollection collection)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến 
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                //Gán giá trị cho đối tượng được tạo mới (kh)
+
+                TaiKhoan kh = db.TaiKhoans.SingleOrDefault(n => n.TenDangNhap == tendn && n.MatKhau == matkhau);
+                if (kh != null)
+                {
+                    // ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoan"] = kh;
+                    return RedirectToAction("LoginIndex","LoginHome");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
             return View();
         }
     }
